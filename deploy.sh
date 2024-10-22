196ad0d1c7ad434a68a9e1f4afd65ee7f36d9571
Ben Chaplin <benchaplin@protonmail.ch>
2024-10-22 01:25:56 +0000

    add delete flag to rsync deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 TARGET_DIR="/var/www/ben-chaplin.com"
 
-sudo rsync -av --exclude='deploy.sh' --exclude='.git' ./ $TARGET_DIR
+sudo rsync -av --delete --exclude='deploy.sh' --exclude='.git' ./ $TARGET_DIR
 sudo chown -R www-data:www-data $TARGET_DIR
 sudo chmod -R 755 $TARGET_DIR
 
